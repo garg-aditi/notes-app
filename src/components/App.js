68c122f69fcd2b1213ca61f90abd85c9d905b684
aditi <garg.aditi6@tcs.com>
2021-08-13 07:01:43 +0530

    added  dynamic notes feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
-
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import notes from "../notes";
-import Note from './Note';
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+ function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
   return (
     <div>
       <Header />
-     
+      <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
         return (
           <Note
@@ -18,6 +33,7 @@ function App() {
             id={index}
             title={noteItem.title}
             content={noteItem.content}
+            onDelete={deleteNote}
           />
         );
       })}
